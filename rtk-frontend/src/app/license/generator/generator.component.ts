9fee14867765a20b008641bd97d6501670f27aa5
Manuel Rodriguez <manrodri92@gmail.com>
2019-03-30 03:10:47 +0300

    Fix Deleted unnecessary functions

--- rtk-frontend/src/app/license/generator/generator.component.ts
+++ rtk-frontend/src/app/license/generator/generator.component.ts
@@ -1,63 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  FormArray,
-  Validators
-} from '@angular/forms';
-// this.form = new FormGroup({
-//   skills: new FormArray([new FormControl(true), new FormControl(false)])
-// });
+import { Component, OnInit } from "@angular/core";
+import { RestService } from "src/app/services/rest.service";
+
 @Component({
-  selector: 'app-generator',
-  templateUrl: './generator.component.html',
-  styleUrls: ['./generator.component.scss']
+  selector: "app-generator",
+  templateUrl: "./generator.component.html",
+  styleUrls: ["./generator.component.scss"]
 })
 export class GeneratorComponent implements OnInit {
-  form;
-  user = {
-    skills: [
-      { name: 'JS', selected: true, id: 12 },
-      { name: 'CSS', selected: false, id: 2 }
-    ]
-  };
-  ngOnInit() {}
+  constructor(private rest: RestService) {}
 
-
-  constructor(
-    private fb: FormBuilder
-    ) {
-    console.clear();
-
-    this.form = this.fb.group({
-      skills: this.buildSkills()
-    });
-
-    console.log(this.form.get('skills'));
+  ngOnInit() {
+    this.loadObjects();
   }
 
-  get skills(): FormArray {
-    return this.form.get('skills') as FormArray;
-  }
-
-  buildSkills() {
-    const arr = this.user.skills.map(skill => {
-      return this.fb.control(skill.selected);
+  // Get Protected objects
+  loadObjects() {
+    return this.rest.getObjects().subscribe((data: {}) => {
+      console.log(data);
     });
-    return this.fb.array(arr);
-  }
-
-  submit(value) {
-    const f = Object.assign({}, value, {
-      skills: value.skills.map((s, i) => {
-        return {
-          id: this.user.skills[i].id,
-          selected: s
-        };
-      })
-    });
-
-    console.log(f);
   }
 }
