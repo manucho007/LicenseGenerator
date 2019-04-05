ef7b411de7a699d5636025b230fedce13cb448a7
KhavratovMV <m.khavratov@innopolis.ru>
2019-04-05 15:56:40 +0300

    Just add component tree

--- rtk-frontend/src/app/app.component.ts
+++ rtk-frontend/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rtk-frontend';
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
+  options = {};
 }
--- rtk-frontend/src/app/app.module.ts
+++ rtk-frontend/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TreeModule } from 'angular-tree-component';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
