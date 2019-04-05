55619f797da8995ec99de6fb1b70cdeb18efe31f
KhavratovMV <m.khavratov@innopolis.ru>
2019-04-05 19:03:28 +0300

    Fill tree form rest

--- rtk-frontend/src/app/app.component.ts
+++ rtk-frontend/src/app/app.component.ts
@@ -1,35 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {RestService} from "./services/rest.service";
+import {ITreeOptions, TreeComponent} from "angular-tree-component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private rest: RestService) {}
+
+  ngOnInit() {
+    this.loadObjects();
+  }
+
+  loadObjects() {
+    return this.rest.getObjects().subscribe((data: {}) => {
+      const self = this;
+
+      Object.keys(data).map(function(objectKeyElement) {
+        const mapKeys = data[objectKeyElement];
+
+        Object.keys(mapKeys).map(function(mapKeyElement) {
+          const value = mapKeys[mapKeyElement];
+          self.nodes.push(value);
+        });
+      });
+
+      this.tree.treeModel.update();
+    });
+  }
+
+  @ViewChild(TreeComponent)
+  private tree: TreeComponent;
+
   title = 'rtk-frontend';
-  nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      children: [
-        { id: 5, name: 'child2.1' },
-        {
-          id: 6,
-          name: 'child2.2',
-          children: [
-            { id: 7, name: 'subsub' }
-          ]
-        }
-      ]
-    }
-  ];
-  options = {};
+
+  nodes = [];
+
+  options: ITreeOptions = {
+    displayField: 'data',
+    childrenField: 'children',
+    useCheckbox: true
+  };
 }
